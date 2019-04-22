8d5aed386bf5d644e8599b89f70f37a481786cfd
shimech <ut.s.shimizu@gmail.com>
2019-04-22 11:31:26 +0900

    やっと正解

--- 20190406_ABC123/C-Five_Transportations.cpp
+++ 20190406_ABC123/C-Five_Transportations.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main() {
-    unsigned int N, A, B, C, D, E;
+    long N, A, B, C, D, E;
     cin >> N;
     cin >> A;
     cin >> B;
     cin >> C;
     cin >> D;
     cin >> E;
-    unsigned int transport[5] = {A, B, C, D, E}, min = A;
+    long transport[5] = {A, B, C, D, E}, min = A;
     for (int i = 1; i < 5; i++) {
         if (transport[i] < min) {
             min = transport[i];
         }
         else {}
     }
-    cout << N / min + 5 << endl;
+    cout << (N + min - 1) / min + 4 << endl;
     return 0;
 }
