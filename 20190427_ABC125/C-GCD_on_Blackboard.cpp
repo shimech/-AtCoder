a1f930f0a70909dfa6b636947b39aad68e7eec08
shimech <ut.s.shimizu@gmail.com>
2019-04-28 21:33:08 +0900

    TLE

---
+++ 20190427_ABC125/C-GCD_on_Blackboard.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b);
+
+int main() {
+    int N; cin >> N;
+    int A[N]; for (int i = 0; i < N; i++) cin >> A[i];
+    int left, right, max = 0;
+    for (int n = 0; n < N; n++) {
+        left = 0;
+        right = 0;
+        for (int i = 0; i < n; i++) left = gcd(left, A[i]);
+        for (int i = N-1; i > n; i--) right = gcd(right, A[i]);
+        if (gcd(left, right) > max) max = gcd(left, right);
+    }
+    cout << max << endl;
+}
+
+int gcd(int a, int b) {
+    if (a < b) swap(a, b);
+    if (b == 0) return a;
+    int r = a % b;
+    while (r != 0) {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    return b;
+}
