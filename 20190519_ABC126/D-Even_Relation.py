a5c6267ed041e4184d730f4474e7a3c1542965a6
shimech <ut.s.shimizu@gmail.com>
2019-05-20 21:55:47 +0900

    20190519 ABC126

--- 20190519_ABC126/D-Even_Relation.py
+++ 20190519_ABC126/D-Even_Relation.py
@@ -1,26 +1,22 @@
-from operator import itemgetter
-import numpy as np
-
 def main():
     N = int(input())
-    color = [-1] * N
-    comb = [[] for i in range(N)]
+    distance = [-1 for _ in range(N)]
+    edges = [[] for _ in range(N)]
+    nodes = [0]
     for _ in range(N-1):
         u, v, w = map(int, input().split())
-        if w % 2 != 0:
-            comb[u-1].append(v-1)
-            comb[v-1].append(u-1)
-    length = np.array(list(map(len, comb)))
-    index = np.argsort(length)[::-1]
-    for n in index:
-        node = comb[n]
-        if color[n] == -1:
-            color[n] = 0
-        if len(node) > 0:
-            for m in node:
-                color[m] = 1 - color[n]
-    for c in color:
-        print(c)
+        edges[u-1].append((v-1, w))
+        edges[v-1].append((u-1, w))
+    distance[0] = 0
+    while any(d < 0 for d in distance):
+        n = nodes.pop(0)
+        for edge in edges[n]:
+            m, d = edge[0], edge[1]
+            if distance[m] == -1:
+                distance[m] = distance[n] + d
+                nodes.append(m)
+    for d in distance:
+        print(0 if d % 2 == 0 else 1)
 
 
 if __name__ == '__main__':
