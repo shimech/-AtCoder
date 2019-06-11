8aa6108442ae5ca492cdb5c0d868a14e5b0977bc
shimech <ut.s.shimizu@gmail.com>
2019-06-11 18:53:08 +0900

    20190609 ABC129

--- 20190609_ABC129/D-Lamp.cpp
+++ 20190609_ABC129/D-Lamp.cpp
@@ -9,35 +9,33 @@ int main() {
     for (int h = 0; h < H; h++) {
         cin >> grid[h];
     }
-    vector<int> dx{1, 0, -1, 0}, dy{0, 1, 0, -1};
-    int count = 1, ans = 0;
-    int ref_x, ref_y;
+    int L[H][W], R[H][W], U[H][W], D[H][W];
+    int ans = 0;
+    int h_l, w_l, h_r, w_r, h_u, w_u, h_d, w_d;
+    for (int i = 0; i < H * W; i++) {
+        h_l = i / W; w_l = i % W;
+        h_r = i / W; w_r = W-1 - i % W;
+        h_u = i % H; w_u = i / H;
+        h_d = H-1 - i % H; w_d = i / H;
+        if (grid[h_l][w_l] == '#') L[h_l][w_l] = 0;
+        else if (w_l == 0) L[h_l][w_l] = 1;
+        else L[h_l][w_l] = L[h_l][w_l-1] + 1;
+
+        if (grid[h_r][w_r] == '#') R[h_r][w_r] = 0;
+        else if (w_r == W-1) R[h_r][w_r] = 1;
+        else R[h_r][w_r] = R[h_r][w_r+1] + 1;
+
+        if (grid[h_u][w_u] == '#') U[h_u][w_u] = 0;
+        else if (h_u == 0) U[h_u][w_u] = 1;
+        else U[h_u][w_u] = U[h_u-1][w_u] + 1;
+
+        if (grid[h_d][w_d] == '#') D[h_d][w_d] = 0;
+        else if (h_d == H-1) D[h_d][w_d] = 1;
+        else D[h_d][w_d] = D[h_d+1][w_d] + 1;
+    }
     for (int h = 0; h < H; h++) {
         for (int w = 0; w < W; w++) {
-            if (grid[h][w] == '#') {
-                count = 0;
-            }
-            else {
-                count = 1;
-                for (int i = 0; i < 4; i++) {
-                    ref_x = h + dx[i];
-                    ref_y = w + dy[i];
-                    while (true) {
-                        if (ref_x >= 0 && ref_x < H && ref_y >= 0 && ref_y < W) {
-                            if (grid[ref_x][ref_y] == '.') {
-                                count++;
-                                if (i == 0) ref_x++;
-                                else if (i == 1) ref_y++;
-                                else if (i == 2) ref_x--;
-                                else ref_y--;
-                            }
-                            else break;
-                        }
-                        else break;
-                    }
-                }
-            }
-            ans = max(ans, count);
+            ans = max(ans, L[h][w] + R[h][w] + U[h][w] + D[h][w] - 3);
         }
     }
     cout << ans << endl;
