221bf837d59a24009d2a370df3db32993e400c67
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-06-07 13:56:25 +0900

    復習

--- 20210606_ABC204/C_Tour.cpp
+++ 20210606_ABC204/C_Tour.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
+const int MAX_N = 2000;
+bool isAchieved[MAX_N];
+vector<vector<int> > G;
+
+void dfs(int v) {
+  if (isAchieved[v]) return;
+  isAchieved[v] = true;
+  for (int vv : G[v]) {
+    dfs(vv);
+  }
+}
 
 int main() {
   int N, M;
   cin >> N >> M;
-  vector<vector<int> > graph(N, vector<int>(N));
+  G.resize(N);
   int a, b;
   for (int m = 0; m < M; m++) {
     cin >> a >> b;
-    graph.at(a - 1).at(b - 1) = 1;
+    G[a - 1].push_back(b - 1);
   }
-  int answer = N;
-  int now;
-  for (int n = 0; n < N; n++) {
-    queue<int> que;
-    vector<int> isAchieved(N);
-    isAchieved.at(n) = 1;
-    que.push(n);
-    while (!que.empty()) {
-      now = que.front();
-      for (int n_ = 0; n_ < N; n_++) {
-        if (n_ != now && graph.at(now).at(n_) == 1 && isAchieved.at(n_) != 1) {
-          que.push(n_);
-          isAchieved.at(n_) = 1;
-          graph.at(now).at(n_) = 1;
-          answer++;
-        }
-      }
-      que.pop();
-    }
+  int answer = 0;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) isAchieved[j] = false;
+    dfs(i);
+    for (int j = 0; j < N; j++)
+      if (isAchieved[j]) answer++;
   }
   cout << answer << endl;
 }
--- 20210606_ABC204/D_Cooking.cpp
+++ 20210606_ABC204/D_Cooking.cpp
@@ -1,26 +1,32 @@
+#include <cmath>
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
   int N;
   cin >> N;
-  vector<int> T(N);
+  int T[N];
+  int S = 0;
   for (int n = 0; n < N; n++) {
-    cin >> T.at(n);
+    cin >> T[n];
+    S += T[n];
   }
-  sort(T.begin(), T.end());
-  vector<int> O(2);
-  for (int n = 0; n < N; n++) {
-    if (n == 0) {
-      O.at(0) = T.at(n);
-    } else {
-      if (O.at(0) <= O.at(1)) {
-        O.at(0) += T.at(n);
-      } else {
-        O.at(1) += T.at(n);
-      }
+  bool dp[N + 1][S + 1];
+  for (int n = 0; n < N + 1; n++) {
+    for (int s = 0; s < S + 1; s++) {
+      dp[n][s] = (s == 0);
     }
   }
-  cout << max(O.at(0), O.at(1)) << endl;
+  for (int n = 0; n < N; n++) {
+    for (int s = 0; s < S + 1; s++) {
+      dp[n + 1][s] = (dp[n + 1][s] || dp[n][s - T[n]]) && s >= T[n];
+      dp[n + 1][s] = dp[n + 1][s] || dp[n][s];
+    }
+  }
+  for (int s = 0; s < S + 1; s++) {
+    if (dp[N][s] && s >= ceil(S / 2.0)) {
+      cout << s << endl;
+      return 0;
+    }
+  }
 }
